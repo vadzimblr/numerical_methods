d4a04c488207912e89a85f9a9bb292e489ad1d62
Vadzim <vadim.poznyak.wrk@gmail.com>
2023-11-01 09:56:37 +0300

    [laba2]: Refactor function and variable names

--- laba2/Gauss.h
+++ laba2/Gauss.h
@@ -1,15 +1,15 @@
 #pragma once
-void Gauss(double** matrix, int N, int M);
-void SwapLines(int line1, int line2, double** a);
+void gauss(double** matrix, int N, int M);
+void swapLines(int line1, int line2, double** a);
 void getAnswerGauss(double** matrix, int N, int M, double* B);
 
-void Gauss(double** matrix, int N, int M) {
-    for (int k = 0; k < N - 1; k++) {   
+void gauss(double** matrix, int N, int M) {
+    for (int k = 0; k < N - 1; k++) {
         double koef = 0;
         if (matrix[k][k] == 0) {
             for (int i = k + 1; i < N; i++) {
                 if (matrix[i][k] != 0) {
-                    SwapLines(k, i, matrix);
+                    swapLines(k, i, matrix);
                     break;
                 }
             }
@@ -27,11 +27,10 @@ void Gauss(double** matrix, int N, int M) {
             matrix[i][j] /= diagElement;
         }
     }
-    
 }
 
 void getAnswerGauss(double** matrix, int N, int M, double* B) {
-    Gauss(matrix, N, M);
+    gauss(matrix, N, M);
     B[N - 1] = matrix[N - 1][M - 1];
     for (int i = N - 2; i > -1; i--) {
         double sum = 0;
@@ -41,8 +40,8 @@ void getAnswerGauss(double** matrix, int N, int M, double* B) {
         B[i] = matrix[i][N] - sum;
     }
 }
-void SwapLines(int line1, int line2, double** a)
-{
+
+void swapLines(int line1, int line2, double** a) {
     double* tmp = a[line1];
     a[line1] = a[line2];
     a[line2] = tmp;
--- laba2/task2.cpp
+++ laba2/task2.cpp
@@ -3,113 +3,121 @@
 #include "Gauss.h"
 #include <vector>
 using namespace std;
-double get_d1(const vector<double>& x);
-double get_d2(const vector<double>& x, const vector<double>& x_Kplus1);
-double funtion_1(const double& x0_1, const double& x0_2);
-double funtion_2(const double& x0_1, const double& x0_2);
-double J_1dx0_1(const double& x0_1, const double& x0_2);
-double J_1dx0_2(const double& x0_1, const double& x0_2);
-double J_2dx0_1(const double& x0_1, const double& x0_2);
-double J_2dx0_2(const double& x0_1, const double& x0_2);
-void Jakobian_method_1(double** matrix, const vector<double>& x);
-void Jakobian_method_2(double** matrix, const vector<double>& x, const double& M);
-int main()
-{
-	//system("chcp 1251");
-	const double eps = 1e-9;
-	double  d1 = 1, d2 = 1;
-	vector<double> x;	
-	x.push_back(-1.5);
-	x.push_back(1.5);
-	vector<double> x_Kplus1(x);
-	int k = 1;
-	int NIT = 100;
-	int N = 2;
-	int M = 3;
-	double M_parametr = 0.1;
-	double** matrix = new double* [N];
-	for (int i = 0; i < N; i++) {
-		matrix[i] = new double[M];
-	}
-	double* ans = new double[N];
-	while ((d1 > eps || d2 > eps) && k < NIT)
-	{
-		Jakobian_method_1(matrix, x);
-		//Jakobian_method_2(matrix, x, M_parametr);
-		matrix[0][2] = -funtion_1(x[0], x[1]);
-		matrix[1][2] = -funtion_2(x[0], x[1]);
-		getAnswerGauss(matrix, N, M, ans);
-		x_Kplus1[0] += ans[0];
-		x_Kplus1[1] += ans[1];
-		
-		d1 = get_d1(x);
-		d2 = get_d2(x, x_Kplus1);
-		cout << "Итерация:" << k << " d1: " << d1 << " d2: " << d2 << endl;
-		k++;
-		x = x_Kplus1;		
-	}
-	//261886462 0.01
-	//261890459 0.05
-	//261947396 0.1
-	cout << "Решение: x = " << setprecision(25) <<x[0] << ", y = " << x[1] << endl << "Кол-во итераций: " << k << endl;
-	delete[] ans;
-	for (int i = 0; i < N; i++) {
-		delete[] matrix[i];
-	}
-	delete[] matrix;
+
+double getD1(const vector<double>& x);
+double getD2(const vector<double>& x, const vector<double>& xKplus1);
+double function1(const double& x01, const double& x02);
+double function2(const double& x01, const double& x02);
+double J1Dx01(const double& x01, const double& x02);
+double J1Dx02(const double& x01, const double& x02);
+double J2Dx01(const double& x01, const double& x02);
+double J2Dx02(const double& x01, const double& x02);
+void jakobianMethod1(double** matrix, const vector<double>& x);
+void jakobianMethod2(double** matrix, const vector<double>& x, const double& M);
+
+int main() {
+    //system("chcp 1251");
+    const double eps = 1e-9;
+    double d1 = 1, d2 = 1;
+    vector<double> x;
+    x.push_back(-1.5);
+    x.push_back(1.5);
+    vector<double> xKplus1(x);
+    int k = 1;
+    int NIT = 100;
+    int N = 2;
+    int M = 3;
+    double MParametr = 0.1;
+    double** matrix = new double* [N];
+    for (int i = 0; i < N; i++) {
+        matrix[i] = new double[M];
+    }
+    double* ans = new double[N];
+
+    while ((d1 > eps || d2 > eps) && k < NIT) {
+        jakobianMethod1(matrix, x);
+        //jakobianMethod2(matrix, x, MParametr);
+        matrix[0][2] = -function1(x[0], x[1]);
+        matrix[1][2] = -function2(x[0], x[1]);
+        getAnswerGauss(matrix, N, M, ans);
+        xKplus1[0] += ans[0];
+        xKplus1[1] += ans[1];
+
+        d1 = getD1(x);
+        d2 = getD2(x, xKplus1);
+        cout << "Итерация:" << k << " d1: " << d1 << " d2: " << d2 << endl;
+        k++;
+        x = xKplus1;
+    }
+
+    //261886462 0.01
+    //261890459 0.05
+    //261947396 0.1
+    cout << "Решение: x = " << setprecision(25) << x[0] << ", y = " << x[1] << endl << "Кол-во итераций: " << k << endl;
+    delete[] ans;
+    for (int i = 0; i < N; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
 }
-double get_d1(const vector<double>& x) {
-	double f1 = funtion_1(x[0], x[1]);
-	double f2 = funtion_2(x[0], x[1]);
-	if (abs(f1) > abs(f2))
+
+double getD1(const vector<double>& x) {
+    double f1 = function1(x[0], x[1]);
+    double f2 = function2(x[0], x[1]);
+    if (abs(f1) > abs(f2))
 		return abs(f1);
 	else
 		return abs(f2);
+}
 
+double getD2(const vector<double>& x, const vector<double>& xKplus1) {
+    double max = -9999999;
+    for (int i = 0; i < x.size(); i++) {
+        if (abs(xKplus1[i]) < 1) {
+            if (abs(xKplus1[i] - x[i]) > max)
+                max = abs(xKplus1[i] - x[i]);
+        } else {
+            if (abs((xKplus1[i] - x[i]) / xKplus1[i]) > max)
+                max = abs((xKplus1[i] - x[i]) / xKplus1[i]);
+        }
+    }
+    return max;
 }
-double get_d2(const vector<double>& x, const vector<double>& x_Kplus1) {
-	double max = -9999999;
-	for (int i = 0; i < x.size(); i++) {
-		if (abs(x_Kplus1[i]) < 1)
-		{
-			if (abs(x_Kplus1[i] - x[i]) > max)
-				max = abs(x_Kplus1[i] - x[i]);
-		}
-		else
-		{
-			if (abs((x_Kplus1[i] - x[i]) / x_Kplus1[i]) > max)
-				max = abs((x_Kplus1[i] - x[i]) / x_Kplus1[i]);
-		}
-	}
-	return max;
+
+double function1(const double& x01, const double& x02) {
+    return x01 * x01 * x02 * x02 - 3 * x01 * x01 - 6 * x02 * x02 * x02 + 8;
 }
-double funtion_1(const double& x0_1, const double& x0_2) {
-	return x0_1 * x0_1 * x0_2 * x0_2 - 3 * x0_1 * x0_1 - 6 * x0_2 * x0_2 * x0_2 + 8;
+
+double function2(const double& x01, const double& x02) {
+    return x01 * x01 * x01 * x01 - 9 * x02 + 2;
 }
-double funtion_2(const double& x0_1, const double& x0_2) {
-	return x0_1 * x0_1 * x0_1 * x0_1 - 9 * x0_2 + 2;
+
+double J1Dx01(const double& x01, const double& x02) {
+    return 2 * x01 * x02 * x02 - 6 * x01;
 }
-double J_1dx0_1(const double& x0_1, const double& x0_2) {
-	return 2 * x0_1 * x0_2 * x0_2 - 6 * x0_1;
+
+double J1Dx02(const double& x01, const double& x02) {
+    return 2 * x02 * x01 * x01 - 18 * x02 * x02;
 }
-double J_1dx0_2(const double& x0_1, const double& x0_2) {
-	return 2 * x0_2 * x0_1 * x0_1 - 18 * x0_2 * x0_2;
+
+double J2Dx01(const double& x01, const double& x02) {
+    return 3 * x01 * x01 * x01;
 }
-double J_2dx0_1(const double& x0_1, const double& x0_2) {
-	return 3 * x0_1 * x0_1 * x0_1;
+
+double J2Dx02(const double& x01, const double& x02) {
+    return -9;
 }
-double J_2dx0_2(const double& x0_1, const double& x0_2) {
-	return -9;
+
+void jakobianMethod1(double** matrix, const vector<double>& x) {
+    matrix[0][0] = J1Dx01(x[0], x[1]);
+    matrix[0][1] = J1Dx02(x[0], x[1]);
+    matrix[1][0] = J2Dx01(x[0], x[1]);
+    matrix[1][1] = J2Dx02(x[0], x[1]);
 }
-void Jakobian_method_1(double** matrix, const vector<double> &x) {
-	matrix[0][0] = J_1dx0_1(x[0], x[1]);
-	matrix[0][1] = J_1dx0_2(x[0], x[1]);
-	matrix[1][0] = J_2dx0_1(x[0], x[1]);
-	matrix[1][1] = J_2dx0_2(x[0], x[1]);
-}
-void Jakobian_method_2(double** matrix, const vector<double>& x, const double& M) {
-	matrix[0][0] = (funtion_1(x[0]+M*x[0], x[1]) - funtion_1(x[0], x[1]))/ M * x[0];
-	matrix[0][1] = (funtion_1(x[0], x[1] + +M * x[1]) - funtion_1(x[0], x[1])) / M * x[1];
-	matrix[1][0] = (funtion_2(x[0] + M * x[0], x[1]) - funtion_2(x[0], x[1])) / M * x[0];
-	matrix[1][1] = (funtion_2(x[0], x[1] + +M * x[1]) - funtion_2(x[0], x[1])) / M * x[1];;
+
+void jakobianMethod2(double** matrix, const vector<double>& x, const double& M) {
+    matrix[0][0] = (function1(x[0] + M * x[0], x[1]) - function1(x[0], x[1])) / M * x[0];
+    matrix[0][1] = (function1(x[0], x[1] + +M * x[1]) - function1(x[0], x[1])) / M * x[1];
+    matrix[1][0] = (function2(x[0] + M * x[0], x[1]) - function2(x[0], x[1])) / M * x[0];
+    matrix[1][1] = (function2(x[0], x[1] + +M * x[1]) - function2(x[0], x[1])) / M * x[1];
 }
